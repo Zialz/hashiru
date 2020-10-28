8ac6f841f2d0764b9e022714e286a2b1ef16a213
Zialz <loris.alexandre7@etu.univ-lorraine.fr>
2020-10-28 19:02:58 +0100

    Test sans build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Map} from './Components/Map/Map.js'
 function App() {
   return (
     <div className="App">
-      <h1>Hashiru</h1>
+      <h1>Hashiru 28/10 1900</h1>
       <Map/>
     </div>
   );
